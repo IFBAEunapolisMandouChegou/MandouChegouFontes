a0df107f44082de9bd364f02ca08b5d15a77db73
william <rw_oc@hotmail.com>
2017-05-22 23:34:26 -0300

    Classe atualizada

--- src/main/java/br/edu/ifba/eunapolis/mandouchegou/model/Item.java
+++ src/main/java/br/edu/ifba/eunapolis/mandouchegou/model/Item.java
@@ -5,14 +5,17 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * @author William
  */
 
+
 @Entity
-@XmlRootElement
 public class Item implements Serializable {
 
 	private static final long serialVersionUID = 1L;
